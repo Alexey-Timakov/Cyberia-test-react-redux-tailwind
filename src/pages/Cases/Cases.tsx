c986007b37ad44ea2617b7df4a91ec3aead941d9
WIN-18FGUV8VB5F\alix <avtimakov70@gmail.com>
2024-06-07 18:16:43 +0300

    Add simple grid squares

--- src/pages/Cases/Cases.tsx
+++ src/pages/Cases/Cases.tsx
@@ -1,7 +1,22 @@
+import styles from "./Cases.module.scss";
+
 export const Cases = () => {
   return (
-    <div>
+    <div style={{
+      margin: "0 5rem"
+    }}>
       <h1>Cases</h1>
+      <div className={styles.grid}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i => {
+          return (
+            <div style={{
+              border: "0.2rem solid red"
+            }} key={i}>
+              <div>Content</div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 };
